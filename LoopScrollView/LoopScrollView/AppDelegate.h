6dfcf86497b3a7e99a52b84006795067f6e0fc5d
Qixin <qixin@QixintekiMacBook-Pro.local>
2014-12-15 18:20:09 +0800

    first blood

---
+++ LoopScrollView/LoopScrollView/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  LoopScrollView
+//
+//  Created by Qixin on 14/12/15.
+//  Copyright (c) 2014年 Qixin. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ LoopScrollView/LoopScrollView/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  LoopScrollView
+//
+//  Created by Qixin on 14/12/15.
+//  Copyright (c) 2014年 Qixin. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
